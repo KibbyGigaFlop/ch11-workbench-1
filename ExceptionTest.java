f9604726185ab6c4f0357654ca1dc62bc244928c
RANDALIEREN <timothybain99@gmail.com>
2019-04-21 17:06:58 -0400

    Exception test algorithm bench 2

--- ExceptionTest.java
+++ ExceptionTest.java
@@ -1,10 +1,13 @@
 public class ExceptionTest{   
 
    /**
-   This program will output B and D since the variable it's trying to parse
+   This program will output B, D, and E since the variable it's trying to parse
    is not an integer and rather, a string, so it will throw the catch the
    number format exception and handling it with the custom error code
-   "B" and a sentence specifying the exception.
+   "B" and a sentence specifying the exception. The 'finally' feature will
+   execute any other error messages specified in the exception handling process,
+   which will print out 'D'. It then proceeds with the code as normal and prints
+   "E" at the end as that's the last statement before the program ends.
    */
 
 
@@ -24,9 +27,13 @@ public class ExceptionTest{
       
          System.out.println("C");
       
-      }
+      }finally{
       
       System.out.println("D");
+      
+      }
+      
+      System.out.println("E");
    
    }
    
